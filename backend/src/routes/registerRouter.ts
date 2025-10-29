a75418e4d2a82467a59ca7dbde6ed497abff8797
Akash Raj <rajakash3852@gmail.com>
2025-10-30 01:59:06 +0530

    remove routing

--- backend/src/routes/registerRouter.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { registerController } from "../utils/registerController.js";
-
-const registerRouter = Router();
-
-registerRouter.post("/", registerController);
